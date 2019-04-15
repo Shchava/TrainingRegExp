9f772b7c197d95ca22571646a17f7dd18138c60f
Shchava <yarosshc@gmail.com>
2019-04-15 15:21:08 +0300

    add InputNoteBook tests

--- src/test/java/TestInputNoteBook.java
+++ src/test/java/TestInputNoteBook.java
@@ -1,12 +1,34 @@
 import org.junit.jupiter.api.Test;
-import ua.trainig.controller.Controller;
-import ua.trainig.model.Model;
+import ua.trainig.controller.inputNoteBook;
 import ua.trainig.model.NoteBook;
 import ua.trainig.view.View;
 
 import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestInputNoteBook {
+    View testView = new View();
+    String testInputLogin = "login\nІмя\nПрізвище";
+    String untrimmedTestInputLogin = "login  \n  Імя  \n   Прізвище";
+
+    @Test
+    void testLoginInput(){
+        inputNoteBook testNoteBook = new inputNoteBook(new Scanner(testInputLogin),testView);
+        NoteBook resp =   testNoteBook.readNoteBook();
+        assertEquals("login", resp.getLogin());
+        assertEquals("Імя", resp.getName());
+        assertEquals("Прізвище", resp.getSurName());
+    }
+
+    @Test
+    void testLoginInputWithSpaces(){
+        inputNoteBook testNoteBook = new inputNoteBook(new Scanner(untrimmedTestInputLogin),testView);
+        NoteBook resp =   testNoteBook.readNoteBook();
+        assertEquals("login", resp.getLogin());
+        assertEquals("Імя", resp.getName());
+        assertEquals("Прізвище", resp.getSurName());
+
+    }
+
 }
