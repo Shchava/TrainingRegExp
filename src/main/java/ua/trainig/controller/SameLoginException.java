cb6e861e35db21074ea98c0d9095eaff6d984eba
Shchava <yarosshc@gmail.com>
2019-04-15 17:40:09 +0300

    add getter for Repeated

--- src/main/java/ua/trainig/controller/SameLoginException.java
+++ src/main/java/ua/trainig/controller/SameLoginException.java
@@ -3,12 +3,16 @@ package ua.trainig.controller;
 import ua.trainig.model.NoteBook;
 
 public class SameLoginException extends Exception{
-    NoteBook repeated;
+    private NoteBook repeated;
     public SameLoginException(String message, NoteBook repeated){
         super(message);
         this.repeated = repeated;
     }
     public SameLoginException(NoteBook repeated){
         this.repeated = repeated;
+    }
+
+    public NoteBook getRepeated(){
+        return repeated;
     }
 }
