74c826a7ad342c27496961e2d120cf606b98da03
Shchava <yarosshc@gmail.com>
2019-04-15 14:46:30 +0300

    add testSuite for InputNoteBook class

---
+++ src/test/java/TestInputNoteBook.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import ua.trainig.controller.Controller;
+import ua.trainig.model.Model;
+import ua.trainig.model.NoteBook;
+import ua.trainig.view.View;
+
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestInputNoteBook {
+}
