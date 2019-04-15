2fda631c3ca8258d4a152bc412ee21760460a5fc
Shchava <yarosshc@gmail.com>
2019-04-15 14:31:28 +0300

    Change set storage of notebooks to map

--- src/main/java/ua/trainig/model/Model.java
+++ src/main/java/ua/trainig/model/Model.java
@@ -1,16 +1,18 @@
 package ua.trainig.model;
 
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Model {
-    Set<NoteBook> noteBookList = new HashSet<NoteBook>();
+    Map<String,NoteBook> noteBookMap = new HashMap<String, NoteBook>();
 
     public Model(){
     }
 
     public void addNoteBook(NoteBook book){
-        noteBookList.add(book);
+        noteBookMap.put(book.getLogin(),book);
     }
 }
