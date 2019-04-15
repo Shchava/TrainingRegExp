dc9fd3e325c9922617a52304f102dfbdafbf6d22
Shchava <yarosshc@gmail.com>
2019-04-15 15:38:56 +0300

    remove redundant imports

--- src/main/java/ua/trainig/model/Model.java
+++ src/main/java/ua/trainig/model/Model.java
@@ -2,9 +2,7 @@ package ua.trainig.model;
 
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class Model {
     private Map<String,NoteBook> noteBookMap = new HashMap<String, NoteBook>();
