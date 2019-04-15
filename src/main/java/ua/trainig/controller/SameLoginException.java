9ca0cea6fa0586f291c39cfb7e67e66c5f2b81fd
Shchava <yarosshc@gmail.com>
2019-04-15 17:39:13 +0300

    add constructor to SameLoginException

--- src/main/java/ua/trainig/controller/SameLoginException.java
+++ src/main/java/ua/trainig/controller/SameLoginException.java
@@ -4,8 +4,11 @@ import ua.trainig.model.NoteBook;
 
 public class SameLoginException extends Exception{
     NoteBook repeated;
-    SameLoginException(String message, NoteBook repeated){
+    public SameLoginException(String message, NoteBook repeated){
         super(message);
+        this.repeated = repeated;
+    }
+    public SameLoginException(NoteBook repeated){
         this.repeated = repeated;
     }
 }
