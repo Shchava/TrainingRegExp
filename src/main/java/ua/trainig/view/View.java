5641b6f8be53b5edfe5cb7f44b7e83b585304075
Shchava <yarosshc@gmail.com>
2019-04-15 16:09:09 +0300

    remove outdated constants

--- src/main/java/ua/trainig/view/View.java
+++ src/main/java/ua/trainig/view/View.java
@@ -4,15 +4,6 @@ package ua.trainig.view;
 import ua.trainig.model.Model;
 
 public class View {
-    public static final String INPUT_NEXT_NUMBER = "Input number";
-    public static final String WRONG_INPUT_DATA = "Wrong input, reenter ";
-    public static final String RANGE_MESSAGE = "Range where number is : [%d - %d] \n";
-    public static final String NumberIsLesser = "Number %d is lesser than needed";
-    public static final String NumberIsBigger = "Number %d is bigger than needed";
-    public static final String NumberIsGuessed = "Congratulations, number %d guessed";
-    public static final String StatisticAnnouncement = "Actions : ";
-    public static final String AttemptsAnnouncement = "Attempts : ";
-
 
     public View(){
 
